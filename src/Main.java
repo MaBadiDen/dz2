86332fe70f63279dd36dd1a5ecb7ef86662d5f0c
MaBadiDen <kaba4ok.den@gmail.com>
2022-07-16 01:01:25 +0300

    Коммит первая часть второго дз

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int _int = 42;
+        long _long = 21546654L;
+        short _short = 23123;
+        boolean _byte = false;
+        float _float = 1.1111f;
+        double _double = 11.1215;
+        char _char = 'a';
+    }
+
+}
